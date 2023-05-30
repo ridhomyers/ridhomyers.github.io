e1817ae609706dd28f30fbbaff0e047d3a050f39
Ridho Rifai <131375185+ridhomyers@users.noreply.github.com>
2023-05-30 23:12:17 +0700

    Add files via upload

---
+++ dist/script.js
@@ -0,0 +1,22 @@
+// hamburger
+
+const hamburger = document.querySelector('#hamburger');
+const navMenu = document.querySelector('#nav-menu');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('hamburger-active');
+    navMenu.classList.toggle('hidden');
+});
+// Navbar fixed
+
+window.onscroll = function () {
+    const header = document.querySelector('header');
+    const fixedNav = header.offsetTop;
+
+    if (window.scrollY > fixedNav) {
+        header.classList.add('navbar-fixed');
+    } else {
+        header.classList.remove('navbar-fixed');
+    }
+};
+
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: "#0ea5e9",
+        dark: "#0f172a"
+      },
+      screen: {
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+}
+
